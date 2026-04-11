7677395bdf27d4adef23a739db421b08471a8b91
Dinnsen <121753045+Dinnsen@users.noreply.github.com>
2026-04-11 12:23:10 +0200

    Delete tests directory

--- tests/test_migration.py
+++
@@ -1,9 +0,0 @@
-from custom_components.ev_guest import async_migrate_entry
-
-
-async def test_migrate_old_entry(hass, mock_config_entry):
-    mock_config_entry.version = 3
-    ok = await async_migrate_entry(hass, mock_config_entry)
-    assert ok is True
-    assert mock_config_entry.version == 4
-    assert mock_config_entry.minor_version == 0
