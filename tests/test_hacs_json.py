7b2141e1b2bedb5c6257bc240d3709f78ff4e5c4
Dinnsen <121753045+Dinnsen@users.noreply.github.com>
2026-04-11 22:53:07 +0200

    Delete tests directory

--- tests/test_hacs_json.py
+++
@@ -1,8 +0,0 @@
-import json
-from pathlib import Path
-
-
-def test_hacs_json_has_domain_and_name():
-    data = json.loads((Path(__file__).resolve().parents[1] / 'hacs.json').read_text(encoding='utf-8'))
-    assert data['name'] == 'EV Guest'
-    assert data['domains'] == ['ev_guest']
