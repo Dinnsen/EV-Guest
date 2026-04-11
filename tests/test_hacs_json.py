6a006869b5c04eb84222f2d3880c29db00ef14c5
Dinnsen <121753045+Dinnsen@users.noreply.github.com>
2026-04-11 21:41:10 +0200

    Delete tests directory

--- tests/test_hacs_json.py
+++
@@ -1,8 +0,0 @@
-import json
-from pathlib import Path
-
-
-def test_hacs_json_shape():
-    data = json.loads(Path('hacs.json').read_text())
-    assert data['country'] == 'DK'
-    assert data['homeassistant'] == '2026.3.0'
